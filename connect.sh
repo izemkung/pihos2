843973597f0cf0b89d0c307a391788139ebf59b4
Peradat Permjai <izemkung@GMAIL.COM>
2023-03-17 08:56:03 +0700

    Update connect.sh
    Set Defult APN

--- connect.sh
+++ connect.sh
@@ -6,6 +6,6 @@ sudo systemctl enable gpsd.socket
 sudo systemctl start gpsd.socket
 cd /
 cd home/pi/3g
-sudo /home/pi/3g/umtskeeper --sakisoperators "USBINTERFACE='3' OTHER='USBMODEM' USBMODEM='05c6:9003' APN="CUSTOM_APN" CUSTOM_APN="tely360" APN_USER='tely360' APN_PASS='tely360'" --sakisswitches "--sudo --console" --devicename 'U20' --log --nat 'no' --httpserver
+sudo /home/pi/3g/umtskeeper --sakisoperators "USBINTERFACE='3' OTHER='USBMODEM' USBMODEM='05c6:9003' APN="CUSTOM_APN" CUSTOM_APN="internet" APN_USER='tely360' APN_PASS='tely360'" --sakisswitches "--sudo --console" --devicename 'U20' --log --nat 'no' --httpserver
 cd /
 
