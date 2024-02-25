b73e90def785205bfb17c30e6e74a1cb584b3e69
Peradat Permjai <izemkung@GMAIL.COM>
2024-02-25 15:14:59 +0700

    update version

--- GPIO.py
+++ GPIO.py
@@ -151,9 +151,9 @@ def SendStatusFun(message):
     
 
     if flagDetectHW_GPS == True:
-        api += '_HW_Sv3.2.2_'
+        api += '_HW_Sv3.6_'
     else:
-        api += '_UC_Sv3.2.2_'
+        api += '_UC_Sv3.6_'
 
     api += version_config
 
